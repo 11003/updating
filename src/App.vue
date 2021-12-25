2c84ca61f7bd4db46882347ab10a5b872c482eac
Sonder <312095995@qq.com>
2021-12-25 16:52:25 +0800

    隐藏鼠标

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <Loader msg="Welcome to Your Vue.js App"/>
+    <Loader />
   </div>
 </template>
 
@@ -22,6 +22,7 @@ body {
     padding: 0;
     font-family:"Microsoft YaHei",微软雅黑,"Microsoft JhengHei",华文细黑,STHeiti,MingLiu;
     background: rgba(0, 109, 174, 1);
+    cursor: none;
 }
 #app {
     position: relative;
